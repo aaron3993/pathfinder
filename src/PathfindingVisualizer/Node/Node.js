ef8defa33ebd49a88e0340ccd344fa9148cc14fd
aaron3993 <aaron3993@hotmail.com>
2020-08-31 22:45:55 -0400

    Setup functional components and imported css files for Node.js and PathfindingVisualizer.js

---
+++ src/PathfindingVisualizer/Node/Node.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import './Node.css';
+
+export default function Node (props) {
+  //const [] = useState()
+
+  return (
+    <div className="node">
+      
+    </div>
+  )
+}
---
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Node from './Node/Node'
+
+import './PathfindingVisualizer.css';
+
+export default function PathfindingVisualizer (props) {
+  // const []
+
+  return (
+    <div>
+      <Node></Node>
+    </div>
+  )
+}
