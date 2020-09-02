b31cc1d0669c91636a7fdb3d8769ece53a39a8e9
aaron3993 <aaron3993@hotmail.com>
2020-09-01 22:32:42 -0400

    Printed the grid on the screen

--- src/PathfindingVisualizer/Node/Node.js
+++ src/PathfindingVisualizer/Node/Node.js
@@ -7,7 +7,6 @@ export default function Node (props) {
 
   return (
     <div className="node">
-      
     </div>
   )
 }
--- src/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Node from './Node/Node'
 
 import './PathfindingVisualizer.css';
 
 export default function PathfindingVisualizer (props) {
-  // const []
+  const [nodes, setNodes] = useState([])
+
+  useEffect(() => {
+    const grid = [];
+    for (let row = 0; row < 15; row++) {
+      const currentRow = []
+      for (let col = 0; col < 50; col++) {
+        currentRow.push([])
+      }
+      grid.push(currentRow)
+    }
+    setNodes(grid)
+  }, [])
 
   return (
-    <div>
-      <Node></Node>
+    <div className="grid">
+      {nodes.map((row, rowIdx) => {
+        return <div>{row.map((node, nodeIdx) => <Node></Node>)}</div>
+      })}
     </div>
   )
 }
